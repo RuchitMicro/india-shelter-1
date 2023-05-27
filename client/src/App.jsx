a3a481bf36eb03149765c447564bda980a174fae
Shubham-ly <shubham.m@ly.design>
2023-05-27 10:05:34 +0530

    Added styles in Tailwind config Fixed prettier formatting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,5 @@
-import { useEffect, useState } from 'react';
-import { getAllTestList } from './api/test';
-
 function App() {
-  const [pokemonData, setPokemonData] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getAllTestList();
-      setPokemonData(data?.results);
-    }
-    fetchData();
-  }, []);
-
-  return (
-    <div
-      style={{
-        marginTop: '40px',
-        justifyContent: 'space-around',
-        display: 'flex',
-        flexWrap: 'wrap',
-        width: '90%',
-        margin: 'auto',
-      }}
-    >
-      {pokemonData?.map((poke, i) => {
-        return (
-          <div
-            key={poke.name}
-            style={{
-              width: '400px',
-              height: '330px',
-              border: '2px solid #000000',
-              margin: '30px 10px',
-            }}
-          >
-            <div style={{ padding: '5px 10px' }}>
-              <p style={{ fontWeight: 'bold', textTransform: 'capitalize' }}> {poke.name}</p>
-            </div>
-            <img
-              style={{ height: '300px', width: '300px' }}
-              alt='pokemon'
-              src={`https://img.pokemondb.net/artwork/large/${poke.name}.jpg`}
-            />
-          </div>
-        );
-      })}
-      <div></div>
-    </div>
-  );
+  return <div>Hello WOrld!</div>;
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
         primary: {
-          red: "#E33439",
-          black: "#373435",
+          red: '#E33439',
+          black: '#373435',
         },
-        "natural-black": "#212121",
-        secondary: { green: "#147257" },
-        "dark-grey": "#727376",
-        "light-grey": "#96989A",
-        "neutral-white": "#FEFEFE",
-        stroke: "#D9D9D9",
+        'natural-black': '#212121',
+        secondary: { green: '#147257', blue: '#065381' },
+        'dark-grey': '#727376',
+        'light-grey': '#96989A',
+        'neutral-white': '#FEFEFE',
+        stroke: '#D9D9D9',
       },
+    },
+    boxShadow: {
+      'shadow-primary': '0px 0px 3px 0px',
     },
   },
   plugins: [],
