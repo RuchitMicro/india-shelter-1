99c62b8dbab4433a0ca1ff62d35dc71f457b1045
Shubham-ly <shubham.m@ly.design>
2023-05-31 15:58:20 +0530

    Added Header

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import indiaShelterLogo from '../../assets/indiashelter.svg';
+
+const Header = () => {
+  return (
+    <div className='p-4 bg-neutral-white'>
+      <img src={indiaShelterLogo} alt='India Shelter' />
+    </div>
+  );
+};
+
+export default Header;
