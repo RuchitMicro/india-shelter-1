f05023b1b4cefd63f49e4d74fefa48807f1c3415
omkarlydesign <omkar.d@ly.design>
2023-06-02 15:14:10 +0530

    components imported in Personal Details page

--- client/src/pages/lead-generation/PersonalDetails.jsx
+++ client/src/pages/lead-generation/PersonalDetails.jsx
@@ -1,5 +1,22 @@
+import Button from '../../components/Button';
+import OtpInput from '../../components/OtpInput';
+import RangeSlider from '../../components/RangeSlider';
+import TextInput from '../../components/TextInput';
+
 const PersonalDetail = () => {
-  return <h1>Personal Detail</h1>;
+  return (
+    <div className='flex flex-col gap-2'>
+      <h1>Personal Details</h1>
+      <TextInput label={"I want a loan of"} required/>
+      <RangeSlider />
+      <TextInput label={"First Name"} placeholder={"Ex: Suresh, Priya"} required />
+      <TextInput label={"Middle Name"} placeholder={"Ex: Ramji, Sreenath"} required />
+      <TextInput label={"Last Name"} placeholder={"Ex: Swami, Singh"} required />
+      <TextInput label={"Current Pincode"} placeholder={"Ex: 123456"} required />
+      <TextInput label={"Mobile number"} placeholder={"Please enter 10 digit mobile no"} required />
+      <OtpInput label={"Enter OTP"} required/>
+    </div>
+  );
 };
 
 export default PersonalDetail;
