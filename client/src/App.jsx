051aeed3bb2a6bebeebfdabf23bfbec761499ca5
Shubham-ly <shubham.m@ly.design>
2023-06-02 18:38:21 +0530

    Added Datepicker component for testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
+import { useState } from 'react';
+import { DatePicker } from './components';
+
 function App() {
-  return <div>Hello WOrld!</div>;
+  const [startDate, setStartDate] = useState();
+
+  return (
+    <div className='bg-white h-full rss-backdrop min-h-full p-4'>
+      <DatePicker startDate={startDate} setStartDate={setStartDate} />
+    </div>
+  );
 }
 
 export default App;
