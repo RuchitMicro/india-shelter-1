94918b8b5eddd155d129ad7edfa4569b5b7f1203
PrathameshShingateLy <prathamesh@ly.design>
2023-06-15 12:17:29 +0530

    added functions for api integration

--- src/global/index.js
+++ src/global/index.js
@@ -1,21 +1,120 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL;
+const API_LEAD_URL = `${API_URL}/lead`;
 
 const requestOptions = {};
 
+async function getApi() {
+  const res = await axios.get(`${API_URL}`, {}, requestOptions);
+  return res.data;
+}
+
+async function createLead(leadData) {
+  const res = await axios.post(`${API_LEAD_URL}/add`, leadData, requestOptions);
+  return res.data;
+}
+
+async function getAllLeads() {
+  const res = await axios.get(`${API_LEAD_URL}`, {}, requestOptions);
+  return res.data;
+}
+
 async function getLeadByPhoneNumber(phoneNumber) {
   const res = await axios.get(
-    `${API_URL}/lead/get-phone-number/${phoneNumber}`,
+    `${API_LEAD_URL}/get-phone-number/${phoneNumber}`,
     {},
     requestOptions,
   );
   return res.data;
 }
 
-async function createLead(leadData) {
-  const res = await axios.post(`${API_URL}/lead/add`, leadData, requestOptions);
+async function getLeadById(id) {
+  const res = await axios.get(`${API_LEAD_URL}/${id}`, {}, requestOptions);
   return res.data;
 }
 
-export { API_URL, createLead, getLeadByPhoneNumber };
+async function editLeadById(id, leadData) {
+  const res = await axios.patch(`${API_LEAD_URL}/edit/${id}`, leadData, requestOptions);
+  return res.data;
+}
+
+async function deleteLeadById(id) {
+  const res = await axios.delete(`${API_LEAD_URL}/delete/${id}`, requestOptions);
+  return res.data;
+}
+
+async function getPincode(pincode) {
+  const res = await axios.get(`${API_URL}/pincode/${pincode}`, {}, requestOptions);
+  return res.data;
+}
+
+async function getMobileOtp(phoneNumber) {
+  const res = await axios.get(`${API_URL}/lead-mobile-otp/${phoneNumber}`, {}, requestOptions);
+  return res.data;
+}
+
+async function verifyMobileOtp(phoneNumber, leadOtp) {
+  const res = await axios.post(
+    `${API_URL}/lead-mobile-otp/${phoneNumber}`,
+    leadOtp,
+    requestOptions,
+  );
+  return res.data;
+}
+
+async function getEmailOtp(email) {
+  const res = await axios.get(`${API_URL}/lead-email-otp/${email}`, {}, requestOptions);
+  return res.data;
+}
+
+async function verifyEmailOtp(email, leadOtp) {
+  const res = await axios.post(`${API_URL}/lead-mobile-otp/${email}`, leadOtp, requestOptions);
+  return res.data;
+}
+
+async function verifyPan(id, leadPan) {
+  const res = await axios.post(`${API_URL}/pan/${id}`, leadPan, requestOptions);
+  return res.data;
+}
+
+async function getLoanType() {
+  const res = await axios.get(`${API_URL}/loan-type`, {}, requestOptions);
+  return res.data;
+}
+
+async function getPropertyIdentification() {
+  const res = await axios.get(`${API_URL}/property-identification`, {}, requestOptions);
+  return res.data;
+}
+
+async function getProfessionalType() {
+  const res = await axios.get(`${API_URL}/professional-type`, {}, requestOptions);
+  return res.data;
+}
+
+async function getDropDownOptions(dropDownTitle) {
+  const res = await axios.get(`${API_URL}/dropdown/${dropDownTitle}`, {}, requestOptions);
+  return res.data;
+}
+
+export {
+  API_URL,
+  getApi,
+  createLead,
+  getAllLeads,
+  getLeadByPhoneNumber,
+  getLeadById,
+  editLeadById,
+  deleteLeadById,
+  getPincode,
+  getMobileOtp,
+  verifyMobileOtp,
+  getEmailOtp,
+  verifyEmailOtp,
+  verifyPan,
+  getLoanType,
+  getPropertyIdentification,
+  getProfessionalType,
+  getDropDownOptions,
+};
